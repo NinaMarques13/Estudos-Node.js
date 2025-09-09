58b442f004d591b1e9eeebd9448ed7823365e387
Nicolas Marques <nicolaspaiva2001@gmail.com>
2025-09-08 21:27:44 -0300

    incluindo todos os arquivos do node que estão na pasta

---
+++ server.js
@@ -0,0 +1,10 @@
+import {createServer, request} from 'node:http'
+
+const server = createServer((request, response) => {
+        response.write('Hello World!')
+
+        return response.end()
+    } 
+)
+
+server.listen(3333)
