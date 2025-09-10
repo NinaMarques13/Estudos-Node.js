1e8102461a190ba8282b667e301c49c8e625a8ad
Nicolas Marques <nicolaspaiva2001@gmail.com>
2025-09-09 22:21:17 -0300

    adicionando o put, delete, get, post e os arquivos do banco de dados postgres

--- primeiro_projeto/database-memory.js
+++ primeiro_projeto/database-memory.js
@@ -3,18 +3,27 @@ import { randomUUID } from 'node:crypto'
 export class DatabaseMemory{
     #videos = new Map() 
 
+    list() {
+        return Array.from(this.#videos.entries()).map((videoArray)=>{
+            const id = videoArray[0]
+            const data = videoArray[1]
+        
+            return {
+                id,
+                ...data,
+            }
+        
+        })
+    }
     create(video) {
-        const videoID = randomUUID
+        const videoID = randomUUID()
 
         this.#videos.set(videoID, video)
     }
-    list(video) {
-        return this.#videos.values()
-    }
-    update(video) {
+    update(id, video) {
         this.#videos.set(id, video)
     }
-    delete(video) {
+    delete(id) {
         this.#videos.delete(id)
     }
 
